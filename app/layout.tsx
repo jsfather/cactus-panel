9628eddfb5b354afcb59200f35b941c58399b893
jsfather <keyvanmatinrad@outlook.com>
2025-03-28 11:13:39 +0330

    code formatted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,51 @@
-import type {Metadata} from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 import localFont from 'next/font/local';
 
-
 const shabnamFD = localFont({
-    src: [
-        {
-            path: './fonts/Shabnam-Bold-FD.ttf',
-            weight: '700',
-            style: 'normal',
-        },
-        {
-            path: './fonts/Shabnam-Medium-FD.ttf',
-            weight: '500',
-            style: 'normal',
-        },
-        {
-            path: './fonts/Shabnam-FD.ttf',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: './fonts/Shabnam-Light-FD.ttf',
-            weight: '300',
-            style: 'normal',
-        },
-        {
-            path: './fonts/Shabnam-Thin-FD.ttf',
-            weight: '100',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-shabnam-fd',
+  src: [
+    {
+      path: './fonts/Shabnam-Bold-FD.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Shabnam-Medium-FD.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Shabnam-FD.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Shabnam-Light-FD.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Shabnam-Thin-FD.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-shabnam-fd',
 });
 
 export const metadata: Metadata = {
-    title: "پنل دانش آموز",
-    description: "آموزش‌های حضوری و آنلاین رباتیک",
+  title: 'پنل دانش آموز',
+  description: 'آموزش‌های حضوری و آنلاین رباتیک',
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="fa" dir="rtl">
-        <body className={shabnamFD.variable}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="fa" dir="rtl">
+      <body className={shabnamFD.variable}>{children}</body>
+    </html>
+  );
 }
